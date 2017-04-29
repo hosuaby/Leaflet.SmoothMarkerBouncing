aa10129ce125584c6ee44a3983cd0c042e24b5d6
Alexei KLENIN <alexei.klenin@gmail.com>
2017-04-30 00:36:20 +0200

    Add example with awesome-markers

---
+++ debug/scripts/app-awesome.js
@@ -0,0 +1,46 @@
+/**
+ * Main application script.
+ */
+;window.onload = function() {
+
+    var map = L.map('map').setView([48.847547, 2.351074], 14);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        zoomControl: true,
+        maxZoom: 18
+    }).addTo(map);
+
+    /* Limit to Paris area */
+    var bounds = [[48.824384, 2.284298], [48.872054, 2.409782]];
+
+    L.Marker.setBouncingOptions({
+        bounceHeight: 40,
+        bounceSpeed: 60
+    });
+
+    var redMarker = L.AwesomeMarkers.icon({
+        prefix: 'glyphicon',
+        icon: 'leaf',
+        markerColor: 'green'
+    });
+
+    /* 20 normal markers */
+    _.times(20, function() {
+        var lat = _.random(bounds[0][0], bounds[1][0]);
+        var lng = _.random(bounds[0][1], bounds[1][1]);
+
+        var marker = L.marker([lat, lng], { icon: redMarker })
+            .setBouncingOptions({
+                bounceHeight: 20
+            })
+            .on('click', function() {
+                this.toggleBouncing();
+            }).addTo(map);
+    });
+
+    /* Stop all bouncing markers on click on the map */
+    map.on('click', function() {
+        L.Marker.stopAllBouncingMarkers();
+    });
+
+}
